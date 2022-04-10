8075ac9eab0be17c48249559deb3df5e1597c894
abhishekyadav2001 <101312722+abhishekyadav2001@users.noreply.github.com>
2022-04-10 20:55:22 +0530

    Add files via upload

---
+++ P1_Task01_FirstJavaProgam.java
@@ -0,0 +1,11 @@
+package P1;
+
+public class P1_Task01_FirstJavaProgam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello worlds");
+
+	}
+
+}
